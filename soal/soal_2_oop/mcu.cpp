441d40ac7583ce730add30f5bd2f1e963ad79b4a
Jordhy Branenda <ethgalleryin@gmail.com>
2026-03-16 23:55:20 +0700

    feat: add solusi soal 2 oop

--- soal/soal_2_oop/mcu.cpp
+++ soal/soal_2_oop/mcu.cpp
@@ -1,6 +1,34 @@
 #include "mcu.hpp"
 // [TODO] : Implementasikan class robot di file cpp ini
 // [TIPS] : Gunakan MCU::<nama methode> untuk mendefinisikan class-nya
+MCU::MCU(float cpu_speed, int memory, string os, string name, int volt)
+{
+    this->cpu_speed=cpu_speed;
+    this->memory=memory;
+    this->os=os;
+    this->name=name;
+    this->volt=volt;
+
+    cout << "[KONSTRUKTOR MCU] : Objek " << name << " berhasil dibuat." << endl; 
+}
+
+MCU::~MCU()
+{
+    cout << "[DESTRUKTOR MCU] : Objek base " << name << " dibersihkan." << endl;
+}
+
+void MCU::nambah_volt(int penambahan_voltase)
+{
+    volt+=penambahan_voltase;
+    cout << "Voltase " << name << " berhasil dinaikkan ke: " << volt << "V" << endl;
+}
+
+void MCU::ganti_os(string new_os)
+{
+    os=new_os;
+    // saya biarin gini karena (ga ada contoh output di expected output (@github)) or (saya kelewatan)
+}
+
 void MCU::showSpek() {
     cout << "[BASE MCU] : " << name << " | CPU: " << cpu_speed 
          << "GHz | RAM: " << memory << "MB | OS: " << os 
--- soal/soal_2_oop/mcu.hpp
+++ soal/soal_2_oop/mcu.hpp
@@ -1,17 +1,25 @@
 #pragma once
-#include "bits/stdc++.h"
+// #include "bits/stdc++.h" include gini ga support di macOS pak, maaf
+#include <iostream>
+#include <vector>
 using namespace std;
 
 /*[TODO]: Implementasikanlah class berikut didalam file robot.cpp agar main.cpp dapat dijalankan */
 class MCU{
-//[TODO]: Buatlah atribute atribute dalam class ini
-private:
-protected:
-public:
-    MCU(float cpu_speed,int memory,string os,string name,int volt);
-    virtual void showSpek();
-    virtual ~MCU();
-    virtual void nambah_volt(int penambahan_voltase);
-    virtual void ganti_os(string new_os);
+    //[TODO]: Buatlah atribute atribute dalam class ini
+    private:
+    protected:
+        float cpu_speed;
+        int memory;
+        string os;
+        string name;
+        int volt;
+    public:
+        MCU(float cpu_speed,int memory,string os,string name,int volt);
+        virtual ~MCU();
+        
+        virtual void showSpek();
+        virtual void nambah_volt(int penambahan_voltase);
+        virtual void ganti_os(string new_os);
 };
 
